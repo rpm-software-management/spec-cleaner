926dc0928156421ca9abaa819924a65fc6dce50f
Tomas Chvatal <tomas.chvatal@gmail.com>
2013-11-14 18:03:34 +0100

    Bit of shuffling around in preparation for conversion to real python module.

--- Makefile
+++ Makefile
@@ -2,33 +2,37 @@ PREFIX ?= /usr
 BINDIR ?= $(PREFIX)/bin
 DATADIR ?= $(PREFIX)/share
 LIBEXECDIR ?= $(PREFIX)/libexec
+LIBDIR ?= $(PREFIX)/lib
+SITEDIR ?= $(LIBDIR)/python2.7/site-packages
 
 all: pkgconfig_conversions.txt
 
-pkgconfig_conversions.txt: pkgconfig-update.sed pkgconfig-update.sh 
+pkgconfig_conversions.txt: pkgconfig-update.sed pkgconfig-update.sh
 	@sh pkgconfig-update.sh 13.1 > $@
 
-install: pkgconfig_conversions.txt licenses_changes.txt spec-cleaner
+install: bin/spec-cleaner
 	@echo "Installing package to $(DESTDIR)" ; \
 	install -d $(DESTDIR)$(BINDIR) ; \
-	install -m 755 spec-cleaner $(DESTDIR)/$(BINDIR)
-	@install -d $(DESTDIR)$(DATADIR)/spec-cleaner ; \
-	install -m 644 licenses_changes.txt $(DESTDIR)$(DATADIR)/spec-cleaner ; \
-	install -m 644 pkgconfig_conversions.txt $(DESTDIR)$(DATADIR)/spec-cleaner/ ; \
-	install -m 644 excludes-bracketing.txt $(DESTDIR)$(DATADIR)/spec-cleaner/
+	install -m 755 bin/spec-cleaner $(DESTDIR)/$(BINDIR)
+	@install -d $(DESTDIR)$(DATADIR)/spec-cleaner/ ; \
+	install -m 644 data/licenses_changes.txt $(DESTDIR)$(DATADIR)/spec-cleaner/ ; \
+	install -m 644 data/pkgconfig_conversions.txt $(DESTDIR)$(DATADIR)/spec-cleaner/ ; \
+	install -m 644 data/excludes-bracketing.txt $(DESTDIR)$(DATADIR)/spec-cleaner/
 	@install -d $(DESTDIR)$(LIBEXECDIR)/obs/service/ ; \
-	install -m 644 format_spec_file.service $(DESTDIR)$(LIBEXECDIR)/obs/service/ ; \
-	install -m 755 format_spec_file $(DESTDIR)$(LIBEXECDIR)/obs/service/ ; \
+	install -m 755 obs/format_spec_file $(DESTDIR)$(LIBEXECDIR)/obs/service/ ; \
+	install -m 644 obs/format_spec_file.service $(DESTDIR)$(LIBEXECDIR)/obs/service/
+	@install -d $(DESTDIR)$(SITEDIR)/spec_cleaner ; \
+	install -m 755 spec_cleaner/__init__.py  $(DESTDIR)$(SITEDIR)/spec_cleaner/
 
 test: check
 
-check: spec-cleaner pkgconfig_conversions.txt licenses_changes.txt
+check: spec_cleaner/__init__.py
 	@echo "Running tests:" ; \
 	for i in tests/in/*.spec; do \
 		CORRECT="`echo $$i | sed 's|^tests/in|tests/out|'`" ; \
 		NEW="`    echo $$i | sed 's|^tests/in|tests/tmp|'`" ; \
 		TEST="`   echo $$i | sed 's|^tests/in/\(.*\).spec|\1|'`" ; \
-		spec-cleaner -f $$i -o "$$NEW" ; \
+		python spec_cleaner/__init__.py -f $$i -o "$$NEW" ; \
 		echo -n " * test '$$TEST': " ; \
 		if [ "`diff "$$CORRECT" "$$NEW" 2>&1`" ]; then \
 			echo "failed" ; \
--- spec_cleaner/__init__.py
+++ spec_cleaner/__init__.py
@@ -1,44 +1,11 @@
 #!/usr/bin/env python
 # vim: set ts=4 sw=4 et: coding=UTF-8
 
-#
 # Copyright (c) 2013, SUSE LINUX Products GmbH, Nuernberg, Germany
 # All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#  * Redistributions of source code must retain the above copyright notice,
-#    this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution.
-#  * Neither the name of the <ORGANIZATION> nor the names of its contributors
-#    may be used to endorse or promote products derived from this software
-#    without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-#
-# (Licensed under the simplified BSD license)
-#
-# Authors:
-#   Vincent Untz <vuntz@novell.com>
-#   Pavol Rusnak <prusnak@opensuse.org>
-#   Petr Uzel <petr.uzel@suse.cz>
-#   Tomas Cech <tcech@suse.cz>
-#   Tomas Chvatal <tchvatal@suse.cz>
-#
+# See COPYING for details.
+
+__version__ = '0.3'
 
 import os
 import sys
@@ -77,7 +44,8 @@ global spec
 
 def open_datafile(FILE):
     try:
-        f = open('{0}/{1}'.format(os.path.dirname(os.path.realpath(__file__)), FILE), 'r')
+        # the .. is appended as we are in spec_cleaner sub_folder
+        f = open('{0}/{1}'.format(os.path.dirname(os.path.realpath(__file__ + '/../data/')), FILE), 'r')
     except IOError:
         f = open('/usr/share/spec-cleaner/{0}'.format(FILE), 'r')
     return f
@@ -166,7 +134,7 @@ def replace_preamble_macros(s):
 
 def read_licenses_changes():
     licenses = {}
-    
+
     f = open_datafile(LICENSES_CHANGES)
     # ignore first line containing 'First line' (WTF?)
     f.readline()
@@ -944,9 +912,9 @@ class RpmSpecCleaner:
         self.diff_prog = diff_prog
 
         spec_macrofuncs = self.find_macros_with_arg(specfile)
-        
+
         f = open_datafile(BRACKETING_EXCLUDES)
-        
+
         keywords= []
         for line in f:
             keywords.append(line[:-1])
@@ -1114,7 +1082,6 @@ The results it produces should always be checked by someone since it is not and 
 
 if __name__ == '__main__':
     try:
-        res = main(sys.argv)
-        sys.exit(res)
+        sys.exit(main(sys.argv))
     except KeyboardInterrupt:
         pass
