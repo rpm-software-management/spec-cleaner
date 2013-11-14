749d561e4d5fba84cf9b6c51b421222dbf40585a
Tomas Chvatal <tomas.chvatal@gmail.com>
2013-11-14 19:18:46 +0100

    Start separation. Now cleaned just the init and exception files.

---
+++ spec_cleaner/rpmexception.py
@@ -0,0 +1,8 @@
+# vim: set ts=4 sw=4 et: coding=UTF-8
+
+class RpmException(Exception):
+    """
+    Class wrapping Exception class so if needed we can do more
+    trickery in here
+    """
+    pass
