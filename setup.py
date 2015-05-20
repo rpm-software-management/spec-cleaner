3310195b7b61891a5082ef67d49b349da37c4d76
Tomáš Chvátal <tomas.chvatal@gmail.com>
2015-05-20 13:58:02 +0200

    Fix warnings around setup.py

--- setup.py
+++ setup.py
@@ -5,41 +5,37 @@ from setuptools import setup
 import glob
 from spec_cleaner import __version__
 
-test_requires = [
-    'mock',
-    'nose',
-]
-
 setup(
     name='spec_cleaner',
-    description = 'RPM .spec files cleaner',
-    long_description = 'Command-line tool for cleaning various formatting' +
+    description='RPM .spec files cleaner',
+    long_description='Command-line tool for cleaning various formatting' +
         'errors in RPM .spec files',
-    url = 'https://github.com/openSUSE/spec-cleaner',
-    download_url = 'https://github.com/openSUSE/spec-cleaner',
+    url='https://github.com/openSUSE/spec-cleaner',
+    download_url='https://github.com/openSUSE/spec-cleaner',
 
-    version = __version__,
+    version=__version__,
 
-    author = 'Tomáš Chvátal',
-    author_email = 'tchvatal@suse.cz',
+    author='Tomáš Chvátal',
+    author_email='tchvatal@suse.cz',
 
-    maintainer = 'Tomáš Chvátal',
-    maintainer_email = 'tchvatal@suse.cz',
+    maintainer='Tomáš Chvátal',
+    maintainer_email='tchvatal@suse.cz',
 
-    license = 'License :: OSI Approved :: BSD License',
-    platforms = ['Linux'],
-    keywords = ['SUSE', 'RPM', '.spec', 'cleaner'],
+    license='License :: OSI Approved :: BSD License',
+    platforms=['Linux'],
+    keywords=['SUSE', 'RPM', '.spec', 'cleaner'],
 
-    tests_require=test_requires,
+    tests_require=['mock', 'nose'],
+
     test_suite="nose.collector",
 
-    packages = ['spec_cleaner'],
+    packages=['spec_cleaner'],
 
     data_files=[('/usr/lib/obs/service/', glob.glob('obs/*')),
                 ('/usr/share/spec-cleaner/', glob.glob('data/*')),
     ],
 
-    entry_points = {
+    entry_points={
         'console_scripts': ['spec-cleaner = spec_cleaner:main']},
 )
 
