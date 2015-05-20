567f61daab23bc26f239fe039440e8a020ffc9f1
Tomáš Chvátal <tomas.chvatal@gmail.com>
2015-05-20 13:30:11 +0200

    Few whitespace issues.

--- spec_cleaner/rpmregexp.py
+++ spec_cleaner/rpmregexp.py
@@ -6,13 +6,13 @@ import os
 from .fileutils import FileUtils
 
 class Singleton(object):
-  def __init__(self, klass):
-    self.klass = klass
-    self.instance = None
-  def __call__(self, *args, **kwds):
-    if self.instance == None:
-      self.instance = self.klass(*args, **kwds)
-    return self.instance
+    def __init__(self, klass):
+        self.klass = klass
+        self.instance = None
+    def __call__(self, *args, **kwds):
+        if self.instance is None:
+            self.instance = self.klass(*args, **kwds)
+        return self.instance
 
 @Singleton
 class RegexpSingle(object):
