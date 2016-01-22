b438a9f883749636c24aff48e336660d331a7a43
Tomáš Chvátal <tchvatal@suse.com>
2016-01-22 14:37:02 +0100

    Fix issue 74: do not insert empty lines after oneline scriptlet

--- spec_cleaner/rpmscriplets.py
+++ spec_cleaner/rpmscriplets.py
@@ -12,10 +12,16 @@ class RpmScriptlets(Section):
     def output(self, fout, newline=True):
         # if we have 2 or 3 lines where last one is empty
         nolines = len(self.lines)
+        if nolines == 1:
+            newline=False
         if nolines == 2 or (nolines == 3 and self.lines[2] == ''):
-            if self.lines[1] == '/sbin/ldconfig':
-                pkg = self.lines[0]
-                self.lines = []
-                self.lines.append('{0} -p /sbin/ldconfig'.format(pkg))
-                self.lines.append('')
+            newline=False
+            # if we have two lines and the 2nd is just whitespace, drop it
+            if self.lines[0] != '' and self.lines[1] == '':
+                self.lines.pop()
+            if len(self.lines) >= 2:
+                if self.lines[1] == '/sbin/ldconfig':
+                    pkg = self.lines[0]
+                    self.lines = []
+                    self.lines.append('{0} -p /sbin/ldconfig'.format(pkg))
         Section.output(self, fout, newline)
--- tests/out/scriplets.spec
+++ tests/out/scriplets.spec
@@ -18,10 +18,8 @@
 
 %post -n %{libname} -p /sbin/ldconfig
 %post -p /sbin/ldconfig
-
 %post
 /sbin/ldconfig
 someothercommand
 
 %post -n %{_libname} -p /sbin/ldconfig
-
