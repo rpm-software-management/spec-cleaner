d4c338b3288b37c1a6099362ce229c67c5d79034
Tomáš Chvátal <tomas.chvatal@gmail.com>
2016-02-02 21:20:28 +0100

    Add minimal case for rpmscriptlets.

--- spec_cleaner/rpmscriplets.py
+++ spec_cleaner/rpmscriplets.py
@@ -10,6 +10,11 @@ class RpmScriptlets(Section):
     '''
 
     def output(self, fout, newline=True):
+        if not self.minimal:
+            newline = self._collapse_multiline_ldconfig(newline)
+        Section.output(self, fout, newline)
+
+    def _collapse_multiline_ldconfig(self, newline):
         # if we have 2 or 3 lines where last one is empty
         nolines = len(self.lines)
         if nolines == 1:
@@ -24,4 +29,4 @@ class RpmScriptlets(Section):
                     pkg = self.lines[0]
                     self.lines = []
                     self.lines.append('{0} -p /sbin/ldconfig'.format(pkg))
-        Section.output(self, fout, newline)
+        return newline
--- tests/out-minimal/scriplets.spec
+++ tests/out-minimal/scriplets.spec
@@ -17,9 +17,14 @@
 
 
 %post -n %{libname} -p /sbin/ldconfig
-%post -p /sbin/ldconfig
+
+%post
+/sbin/ldconfig
+
 %post
 /sbin/ldconfig
 someothercommand
 
-%post -n %{_libname} -p /sbin/ldconfig
+%post -n %{_libname}
+/sbin/ldconfig
+
