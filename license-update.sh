083f2ae4b568ed934851e5e7cf7a6e9966b9a50f
Tomáš Chvátal <tomas.chvatal@gmail.com>
2016-04-26 09:08:38 +0200

    Store license_exceptions list wrt issue #116

--- license-update.sh
+++ license-update.sh
@@ -55,6 +55,13 @@ unset IFS
 rm licenses_changes.raw
 ) > README.md
 
+for i in `w3m -dump -cols 1000 http://spdx.org/licenses/exceptions-index.html | grep "License Exception Text" | sed -e 's, *Y *License Exception Text,,; s, *License Exception Text,,; s,\s* \([^ ]*\)$,:\1,' | cut -d: -f2`; do
+    echo "$i" >> license_exceptions.ntxt ;
+done
+
+cat license_exceptions.ntxt | sort -u -o data/licences_exceptions.txt
+rm license_exceptions.ntxt
+
 cat licenses_changes.ntxt licenses_changes.ptxt | sort -u -o licenses_changes.stxt
 ( echo "First line" ; cat licenses_changes.stxt ) > data/licenses_changes.txt
 rm licenses_changes.ntxt licenses_changes.stxt licenses_changes.ptxt
