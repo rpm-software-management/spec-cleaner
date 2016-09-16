ef032a037185137f5f8fd139bc39eff5690ee793
Tomáš Chvátal <tchvatal@suse.com>
2016-09-16 14:46:07 +0200

    Detect config data in homedir if present, wrt issue #152

--- spec_cleaner/fileutils.py
+++ spec_cleaner/fileutils.py
@@ -23,8 +23,11 @@ class FileUtils(object):
         Used all around so kept glob here for importing.
         """
 
+        homedir = os.getenv('HOME') + '/.local/'
+
         possible_paths = [
             '{0}/../data/{1}'.format(os.path.dirname(os.path.realpath(__file__)), name),
+            '{0}/share/spec-cleaner/{1}'.format(homedir, name),
             '{0}/share/spec-cleaner/{1}'.format(sysconfig.get_path('data'), name),
             '{0}/share/spec-cleaner/{1}'.format(sys.prefix, name),
         ]
