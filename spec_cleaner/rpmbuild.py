cfbf910e112048ea50b38fbc6b3ae407322ee129
Tomáš Chvátal <tomas.chvatal@gmail.com>
2017-04-07 22:36:04 +0200

    Add fix for the issue #178

--- spec_cleaner/rpmbuild.py
+++ spec_cleaner/rpmbuild.py
@@ -18,15 +18,32 @@ class RpmBuild(RpmCheck):
         # if user uses cmake/configure directly just recommend him using the macros
         # but check on the multiline entry and do not ammend that
         if not self.minimal:
-            if (not self.previous_line \
-               or (not self.previous_line.startswith('#') \
-               and not self.previous_line.endswith('\\'))) \
-               and self.reg.re_configure.match(line):
-                self.lines.append('# FIXME: you should use the %%configure macro')
-            if (not self.previous_line \
-               or (not self.previous_line.startswith('#') \
-               and not self.previous_line.endswith('\\'))) \
-               and self.reg.re_cmake.match(line):
-                self.lines.append('# FIXME: you should use %%cmake macros')
+            self._comment_macro_calls(line)
 
         RpmCheck.add(self, line)
+
+    def _comment_macro_calls(self, line):
+        """
+        Add comment if the package uses direct call instead of macro
+        """
+        if self.reg.re_configure.match(line):
+            msg = '# FIXME: you should use the %%configure macro'
+        elif self.reg.re_cmake.match(line):
+            msg = '# FIXME: you should use %%cmake macros'
+        else:
+            return
+
+        position = len(self.lines)
+        if self.previous_line.endswith('\\'):
+            # Backtrack until we are on top of multiline command
+            for i in reversed(self.lines):
+                if i.endswith('\\'):
+                    position -= 1
+                else:
+                    # we stop backtracking on the first
+                    # non-backslashed line
+                    break
+        # Check if on the position above is no comment
+        # if not add our message
+        if not self.lines[position - 1].startswith('#'):
+            self.lines.insert(position, msg)
