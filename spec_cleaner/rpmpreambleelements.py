cc835d8dae6968dbce0ab39ed5443c9c481f2012
Tomáš Chvátal <tomas.chvatal@gmail.com>
2017-05-09 19:08:34 +0200

    Fix typo

--- spec_cleaner/rpmpreambleelements.py
+++ spec_cleaner/rpmpreambleelements.py
@@ -1,7 +1,7 @@
 # vim: set ts=4 sw=4 et: coding=UTF-8
 
 from .rpmhelpers import sort_uniq, add_group, find_pkgconfig_statement, find_pkgconfig_declaration, fix_license
-from .rpmexceptions import RpmException
+from .rpmexception import RpmException
 
 class RpmPreambleElements(object):
     """
