dc716183d6072be2f30d0573d7e4e38c125685a2
Tomáš Chvátal <tomas.chvatal@gmail.com>
2018-01-11 22:11:14 +0100

    Rewrite fileutils tests in pytest

--- tests/fileutils-tests.py
+++ tests/fileutils-tests.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import unittest
-from nose.tools import raises
+import pytest
 
 from spec_cleaner.fileutils import FileUtils
 from spec_cleaner import RpmException
 
-class TestFileutils(unittest.TestCase):
+class TestFileutils(object):
 
     """
     We run few tests to ensure fileutils class works fine
     """
 
-    def setUp(self):
+    def setup_class(self):
         """
         Declare global scope variables for further use.
         """
         self.fileutils = FileUtils()
 
-    @raises(RpmException)
     def test_open_assertion(self):
-        self.fileutils.open('missing-file.txt', 'r')
+        with pytest.raises(RpmException):
+            self.fileutils.open('missing-file.txt', 'r')
 
-    @raises(RpmException)
     def test_open_datafile_assertion(self):
-        self.fileutils.open_datafile('missing-file.txt')
+        with pytest.raises(RpmException):
+            self.fileutils.open_datafile('missing-file.txt')
 
     def test_open(self):
         self.fileutils.open('tests/fileutils-tests.py', 'r')
         self.fileutils.close()
-        self.assertEqual(None, self.fileutils.f)
+        assert self.fileutils.f is None
 
     def test_open_datafile(self):
         self.fileutils.open_datafile('excludes-bracketing.txt')
         self.fileutils.close()
-        self.assertEqual(None, self.fileutils.f)
+        assert self.fileutils.f is None
