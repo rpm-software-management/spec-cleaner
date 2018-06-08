5bd7a38b85cd5179508ce5cffcb79e512d04e5d8
Tomáš Chvátal <tchvatal@suse.com>
2018-06-08 10:44:18 +0200

    Fix __main__.py to be useful for testing

--- spec_cleaner/__main__.py
+++ spec_cleaner/__main__.py
@@ -1,18 +1,10 @@
-from __future__ import absolute_import
-
-import os
 import sys
+import os
 
-# If we are running from a wheel, add the wheel to sys.path.
-if __package__ == '':
-    # __file__ is spec-cleaner-*.whl/spec_cleaner/__main__.py.
-    # First dirname call strips of '/__main__.py', second strips off '/spec_cleaner'.
-    # Resulting path is the name of the wheel itself.
-    # Add that to sys.path so we can import spec_cleaner.
-    path = os.path.dirname(os.path.dirname(__file__))
-    sys.path.insert(0, path)
+path = os.path.dirname(os.path.dirname(__file__))
+sys.path.insert(0, path)
 
-import spec_cleaner
+from spec_cleaner import main
 
 if __name__ == '__main__':
-    sys.exit(spec_cleaner.main())
+    sys.exit(main())
