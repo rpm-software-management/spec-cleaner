feaa7564c9d2990d15a014602b0913b125cd986a
Kristyna Streitova <kstreitova@suse.com>
2019-11-11 15:30:23 +0100

    Bump the version to 1.1.7

--- spec_cleaner/__init__.py
+++ spec_cleaner/__init__.py
@@ -14,7 +14,7 @@ from .rpmcleaner import RpmSpecCleaner
 from .rpmexception import RpmException, RpmWrongArgs
 
 
-__version__ = '1.1.6'
+__version__ = '1.1.7'
 
 
 def process_args(argv: List[str]) -> Dict[str, Any]:
