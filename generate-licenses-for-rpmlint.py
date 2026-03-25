323c9c35a166b2cc596b3491d7a8d6a31e59fb58
Dirk Müller <dirk@dmllr.de>
2026-03-25 18:37:18 +0100

    Update licenses from SPDX, remove exceptions
    Unfortunately this is a fork of the code maintained in
    obs-service-format_spec_file which is unfortunate. update it
    with the latest changes

--- generate-licenses-for-rpmlint.py
+++ generate-licenses-for-rpmlint.py
@@ -3,28 +3,29 @@
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description='Generate TOML configuration file with valid licenses.')
+parser = argparse.ArgumentParser(
+    description='Generate TOML configuration file with valid licenses.'
+)
 parser.add_argument('output', help='Output file')
 parser.add_argument('-s', '--suse', action='store_true', help='Add SUSE exceptions')
 
 args = parser.parse_args()
-
-SUSE_EXCEPTIONS = """
-AGPL-3.0 AGPL-3.0+ GFDL-1.1 GFDL-1.1+ GFDL-1.2 GFDL-1.2+ GFDL-1.3 GFDL-1.3+ GPL-3.0-with-GCC-exception \
-GPL-2.0-with-classpath-exception GPL-2.0-with-font-exception SUSE-LGPL-2.1+-with-GCC-exception SUSE-NonFree \
-GPL-1.0+ GPL-1.0 GPL-2.0+ GPL-2.0 GPL-3.0+ GPL-3.0 LGPL-2.0 LGPL-2.0+ LGPL-2.1+ LGPL-2.1 LGPL-3.0+ LGPL-3.0
-"""
 
 with open(args.output, 'w') as wfile:
     script_name = os.path.basename(__file__)
     wfile.write('# Generated with %s script from spec-cleaner:\n' % script_name)
     wfile.write('# URL: https://github.com/rpm-software-management/spec-cleaner\n\n')
     wfile.write('ValidLicenses = [\n')
+    suse_exceptions = []
     for line in open('data/licenses_changes.txt').readlines():
-        name = line.strip().split('\t')[0]
-        wfile.write(f'    "{name}",\n')
+        if line.startswith('First line'):
+            continue
+        old_name, new_name = line.strip().split('\t')
+        wfile.write(f'    "{old_name}",\n')
+        if new_name.startswith('LicenseRef-SUSE') or new_name.startswith('SUSE-'):
+            suse_exceptions.append(new_name)
     if args.suse:
         wfile.write('    # SUSE EXCEPTIONS\n')
-        for name in SUSE_EXCEPTIONS.strip().split(' '):
+        for name in suse_exceptions:
             wfile.write(f'    "{name}",\n')
     wfile.write(']\n')
